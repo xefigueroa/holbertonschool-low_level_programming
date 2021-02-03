2e4686a86446e506d124f47037b82164411099ea
xefigueroa <x.e.figueroa@gmail.com>
2021-02-03 20:59:33 +0000

    second try

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,6 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
@@ -13,6 +12,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
-
 	return (0);
 }
