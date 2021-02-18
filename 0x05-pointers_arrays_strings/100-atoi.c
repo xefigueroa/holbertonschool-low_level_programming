63922cafc9124dd039f95080c365fe3f618c1d54
xefigueroa <xefigueroa@github.com>
2021-02-18 02:14:22 +0000

    third try adv task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -20,15 +20,15 @@ int _atoi(char *s)
 		{
 			w = w * -1;
 		}
-
-		if (*s >= '0' && *s <= '9')
+		else if (*s >= '0' && *s <= '9')
 		{
-			while (*s >= '0' && *s <= '9')
-			{
-				num = num * 10 + (*s - '0');
-			}
+			num = num * 10 + (*s - '0');
+		}
+		else if (num > 0)
+		{
 			break;
 		}
+
 	} while (*s++);
 
 	return (num * w);
