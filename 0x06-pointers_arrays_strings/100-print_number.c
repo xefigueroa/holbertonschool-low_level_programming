24543a84e95847b5c46f5da540622a701b32d51d
xefigueroa <xefigueroa@github.com>
2021-02-19 13:19:32 +0000

    advanced task

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints an integer
+ * @n: number to print
+ *
+ * Return: Always 0
+ */
+
+void print_number(int n)
+{
+	unsigned int v = n;
+
+	if ( n < 0)
+	{
+		_putchar('-');
+		v = -v;
+	}
+
+	if ((v / 10) > 0)
+	{
+		print_number(v / 10);
+	}
+
+	_putchar((v % 10) + '0');
+}
+
