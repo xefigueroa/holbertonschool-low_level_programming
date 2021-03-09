22e99b1468cc06790e78622c148602f5920c4ace
xefigueroa <xefigueroa@github.com>
2021-03-09 03:29:09 +0000

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints name of file that it was compiled from
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
