070e543d95358fb475c4ac2bbd954b5edd13f320
xefigueroa <xefigueroa@github.com>
2021-06-05 07:22:15 +0000

    0x1A-hash_tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates hash table.
+ * @size: size of array.
+ *
+ * Return: pointer to the newly created hash table or NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int v;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL) /* return condition */
+	{
+		return (NULL);
+	}
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	
+	if (ht->array == NULL) /* return condition */
+	{
+		return (NULL);
+	}
+	
+	for (v = 0; v < size; v++)
+	{
+		ht->array[v] = NULL;
+	}
+
+	return (ht);
+}
