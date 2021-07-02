fb3f633cd64b6ac1fddfce42f4d92305f05a9ce6
xefigueroa <xefigueroa@github.com>
2021-07-02 07:05:47 +0000

    finally some progress

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,48 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: hash table you want to add or update the key/value to
+ * @key: key - cannot be an empty string.
+ * @value: value associated with key.
+ *
+ * Return: fail 0, Otherwise 1.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *val_copy;
+	unsigned long int index, i;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+	val_copy = strdup(value);
+	if (val_copy == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = val_copy;
+			return (1);
+		}
+	}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(val_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = val_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
+}
